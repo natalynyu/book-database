e1f65c55a645c7411e8d5326e10ed084aaceb841
Natalyn Yu <natalyn.yu@gmail.com>
2018-07-15 21:09:52 -0400

    Validate the form
    The form is now validated using the 'required' attribute in the HTML as well as in the JS if 'required' is not supported.

--- library.php
+++ library.php
@@ -6,22 +6,38 @@
 </head>
 <body>
 <h3>Add a book to our database</h3>
-<form method="get" action="saveBook.php">
-Title: <input type="text" name="title"><br><br>
-Author: <input type="text" name="author"><br><br>
+<form method="get" action="saveBook.php" id="form">
+Title: <input type="text" name="title" id="title" required><br><br>
+Author: <input type="text" name="author" id="author" required><br><br>
 Publication Year: 
 <select name="year">
 <?php
 $firstYear = 1800;
 $lastYear = 2020;
 for ($year = $lastYear; $year >= $firstYear; $year--) {
-	echo "<option>".$year."</option>";
+	echo "<option>$year</option>";
 }
 ?>
 </select>
 <br><br>
-
 <input type="submit" value="Add Book">
 </form>
+<script>
+// Validate form in case 'required' attribute is not supported.
+var form = document.getElementById("form");
+var titleField = document.getElementById("title");
+var authorField = document.getElementById("author");
+form.addEventListener("submit", function(event){
+	event.preventDefault();
+	if (titleField.value === "") {
+		alert("Please enter in the book title.");
+		return;
+	} else if (authorField === "") {
+		alert("Please enter in the author's name.");
+		return;
+	}	
+	form.submit();
+});
+</script>
 </body>
 </html>
